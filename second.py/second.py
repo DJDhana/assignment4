393416f7b9acf6bd583557e7a16403e870638106
DJDhana <dhanasekharmarolix@gmail.com>
2023-08-02 11:53:12 +0530

    this is fourth commit

---
+++ second.py/second.py
@@ -0,0 +1,92 @@
+ #Membership in string
+    
+#a = input("enter a name 1:")
+#b = input("enter a name 2:")   
+#if b in a:
+    #print("name is matching")
+#else:
+    #print(" name does not matching")
+
+  # removing space from string
+
+#a = input("enter a text:")
+#print(a.strip())
+
+#a = input("enter a text:")
+#print(len(a))
+#stri_str = a.strip()
+#print(len(stri_str))
+
+
+#a = input("enter a text:")
+#print(len(a))
+#left_strip = a.strip()
+#print(len(left_strip))
+
+
+
+#a = input("enter a text:")
+#print(len(a))
+#right_strip = a.strip()
+#print(len(right_strip))
+
+
+
+## comparing string
+
+#a = input("enter a string:")
+#b = input("enter a string:")
+#if a == b:
+   #print("both names are same")
+#else:
+    #print("both names are different")
+
+
+# find
+#a = "ntr is the world famous actour"
+#print(a.find("t"))
+
+#a = "ntr is the world famous actour"
+#print(a.rfind("t"))
+
+#a = "ntr is the world famous actour"
+#print(a.find("t",7,13))
+
+
+## index  
+
+#a = input("enter name 1:")
+#b = input("enter name 2:")
+#if a.index(b):
+    #print(b,"is matched")
+#else:
+    #print(b,"is not matched")
+
+#d = "Dhanasekhar"
+#j=d.rindex("a")
+#print(j)
+
+#d = "Dhanasekhar"
+#j=d.index("a")
+#print(j)
+
+### count
+
+
+#d = "Dhanasekhar is a python learining"
+#print(d.count("a"))
+
+
+## access 
+#a = "Dhanasekhar"
+#print(a[::-1])
+
+
+#a = "Dhanasekhar"
+#print(a[4])
+
+#a = input("enter a string:")
+#string =" "
+#for i in a:
+    #string = i+string
+#print(string)
